f7346faf4ad81c96f2845fb860833d86a6d0f037
Ansh Payal <anshpayal754@gmail.com>
2024-08-11 13:31:36 +0530

    Fixed Feature icon

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,38 +1,21 @@
-import PropTypes from 'prop-types';
-import { MessageCircle, Zap, Image } from "../assets";
+/* eslint-disable react/prop-types */
+import { Image } from "../assets";
+import { MessageCircle, Zap } from 'lucide-react';
 
-const FEATURES = [
-  {
-    icon: MessageCircle,
-    title: "Share team inboxes",
-    description: "Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop."
-  },
-  {
-    icon: Zap,
-    title: "Deliver instant answers",
-    description: "An all-in-one customer service platform that helps you balance everything your customers need to be happy."
-  }
-];
-
-
-const FeatureItem = ({ icon, title, description }) => (
-  <div className="flex gap-x-4">
-    <div className={`bg-[#F4EBFF] flex items-center justify-center w-32 md:w-36 lg:w-24 h-12 rounded-full`}>
-      <img src={icon} alt={`${title} icon`} className="w-5 h-5" />
+const Feature = ({ icon, title, description }) => {
+  return (
+    <div className="flex items-start gap-x-4">
+      <div className="flex-shrink-0">
+        <div className="bg-[#F4EBFF] rounded-full p-3">
+          {icon}
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <h3 className="text-xl font-medium text-[#101828] ">{title}</h3>
+        <p className="text-base text-[#667085]">{description}</p>
+      </div>
     </div>
-    <div className="flex flex-col gap-y-2">
-      <h3 className={`text-xl font-medium text-[#101828] flex items-center`}>
-        {title}
-      </h3>
-      <p className={`text-base text-[#667085]`}>{description}</p>
-    </div>
-  </div>
-);
-
-FeatureItem.propTypes = {
-  icon: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  );
 };
 
 const FeatureSection = () => {
@@ -47,10 +30,17 @@ const FeatureSection = () => {
               Untitled are a commercial interior design studio. We specialise in customised office design, restaurant design, shop design, and studio design.
             </p>
           </div>
-          <div className="flex flex-col gap-y-12">
-            {FEATURES.map((feature, index) => (
-              <FeatureItem key={index} {...feature} />
-            ))}
+          <div className=" space-y-12">
+            <Feature
+              icon={<MessageCircle className="w-5 h-5 text-[#7F56D9]" />}
+              title="Share team inboxes"
+              description="Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop."
+            />
+            <Feature
+              icon={<Zap className="w-5 h-5 text-[#7F56D9]" />}
+              title="Deliver instant answers"
+              description="An all-in-one customer service platform that helps you balance everything your customers need to be happy."
+            />
           </div>
         </div>
       </div>
