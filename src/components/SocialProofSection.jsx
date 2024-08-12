954772f62c1f2b8f6ab41e9721b696cf0a1cfb1b
Ansh Payal <anshpayal754@gmail.com>
2024-08-12 06:46:10 +0530

    fixed production bug

--- src/components/SocialProofSection.jsx
+++ src/components/SocialProofSection.jsx
@@ -21,9 +21,16 @@ const Logo = ({ src, alt, width, height, smWidth, smHeight, hideOnSmall }) => (
 const SocialProofSection = () => {
   return (
     <div className="bg-[#53389E] text-center pb-24 px-1 sm:px-4 mx-auto">
-      <div className="container mx-auto flex flex-col items-center gap-y-[32px] px-4">
+      <div className=" container mx-auto flex flex-col items-center gap-y-[32px] px-4">
         <p className="text-white font-inter">We&rsquo;ve worked with some great startups</p>
         <div className="grid grid-cols-2 gap-y-4 gap-x-8 justify-center items-center md:flex md:flex-wrap md:justify-center md:gap-x-24 md:gap-y-6 lg:justify-between">
+          {/* <img src={CompanyLogo1} alt="company-logo-1" className="w-[109.5px] h-[36px] sm:w-[146px] sm:h-[48px]" />
+          <img src={CompanyLogo2} alt="company-logo-2" className="w-[126.75px] h-[36px] sm:w-[169px] sm:h-[48px]" />
+          <img src={CompanyLogo3} alt="company-logo-3" className="w-[137.25px] h-[36px] sm:w-[183px] sm:h-[48px]" />
+          <img src={CompanyLogo4} alt="company-logo-4" className="w-[120px] h-[36px] sm:w-[160px] sm:h-[48px]" />
+          <img src={CompanyLogo5} alt="company-logo-5" className="w-[140.25px] h-[36px] sm:w-[187px] sm:h-[48px]" />
+          <img src={CompanyLogo6} alt="company-logo-5" className=" block sm:hidden w-[145.5px] h-[36px]" /> */}
+
           {logos.map((logo, index) => (
             <Logo key={index} {...logo} />
           ))}
